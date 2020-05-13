9364de75454c7e0eeac2a04a0f4cdfcb75022675
leordefreitas <leonardodefreitas1995@yahoo.com.br>
2020-05-13 08:23:19 -0300

    second class

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="wrap">
+        @ Leonardo R. de Freitas
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './styles.scss';
+import { Link } from 'react-router-dom';
+
 import Logo from '../../assets/logo.png';
 
 function Header() {
@@ -7,7 +9,18 @@ function Header() {
     <header className="header">
       <div className="wrap">
         <div className="logo">
-          <img src={Logo} alt="Simple Logo" />
+          <Link to="/">
+            <img src={Logo} alt="Simple Logo" />
+          </Link>
+        </div>
+        <div className="callToActions">
+          <ul>
+            <li>
+              <Link to="/registration">
+                Register
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
